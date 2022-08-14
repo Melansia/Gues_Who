a986f7b9a34850fbc2962d3fa2060c991186e747
Grigori L <lupasco.grigori@gmail.com>
2022-08-14 16:44:14 +0300

    prepared the layout

--- app/src/main/java/com/slt/guesswho/MainActivity.kt
+++ app/src/main/java/com/slt/guesswho/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.slt.guesswho
 
+import android.graphics.Bitmap
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var ivImageStar: ImageView
+
+    private lateinit var btnAnswer1: Button
+    private lateinit var btnAnswer2: Button
+    private lateinit var btnAnswer3: Button
+    private lateinit var btnAnswer4: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ivImageStar = findViewById(R.id.ivImageStar)
+
+        btnAnswer1 =  findViewById(R.id.btnAnswer1)
+        btnAnswer2 =  findViewById(R.id.btnAnswer2)
+        btnAnswer3 =  findViewById(R.id.btnAnswer3)
+        btnAnswer4 =  findViewById(R.id.btnAnswer4)
+
     }
 }
